6d96e575df8368b14ba1188347f2b7c08cad7473
DeadManLogan <grigoroudisdimosthenis@gmail.com>
2022-02-04 22:08:23 +0200

    Added a new note for input text. Styled the text area and the backgorund. Also made the button cooler.

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,7 +1,7 @@
 const AddNote = () => {
     return (
         // this div funcions like a note container but it will be different than the others.
-        <div className = "note 2">
+        <div className = "note new">
             <textarea 
                 rows="8" 
                 columns="10" 
